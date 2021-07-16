85102e525bdf88a79646f1ba8b7ab5f56550be73
Buck <bytao@icloud.com>
2021-07-16 16:28:16 +0800

    up

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; Make startup faster by reducing the frequency of garbage
 ;; collection.
-;; (setq gc-cons-threshold (* 50 1000 1000))
+(setq gc-cons-threshold (* 50 1000 1000))
 
 (require 'package)
 (package-initialize)
@@ -10,9 +10,9 @@
  (expand-file-name "config.org"
 		   user-emacs-directory))
 
-;; (if (file-exists-p (expand-file-name "config.el" user-emacs-directory))
-;;     (load-file (expand-file-name "config.el" user-emacs-directory))
-;;   (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
+(if (file-exists-p (expand-file-name "config.el" user-emacs-directory))
+    (load-file (expand-file-name "config.el" user-emacs-directory))
+  (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
 
 ;; Make gc pauses faster by decreasing the threshold.
-;; (setq gc-cons-threshold (* 2 1000 1000))
+(setq gc-cons-threshold (* 2 1000 1000))
