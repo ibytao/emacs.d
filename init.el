a50a9835a6ced9fdfa2d3507581a72289e88689f
Buck <bytao@icloud.com>
2021-12-12 16:46:14 +0800

    up

--- init.el
+++ init.el
@@ -10,11 +10,6 @@
  (expand-file-name "config.org"
 		   user-emacs-directory))
 
-(global-set-key (kbd "C-x c")
-                (lambda() (interactive)(org-babel-load-file
-                                        (expand-file-name "config.org"
-		                                                  user-emacs-directory))))
-
 (if (file-exists-p (expand-file-name "config.el" user-emacs-directory))
     (load-file (expand-file-name "config.el" user-emacs-directory))
   (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
